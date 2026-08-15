4e18d14d9c186b0d850b32cd2e5f887f4a7f4d49
Sharmila B <sharmi29895@gmail.com>
2026-08-15 14:03:14 +0530

    update tourism_project/model_building/data_register.py

--- tourism_project/model_building/data_register.py
+++ tourism_project/model_building/data_register.py
@@ -39,19 +39,34 @@ missing_columns = [
 if missing_columns:
     raise ValueError(f"Missing expected columns: {missing_columns}")
 
+# Printing all the information present in the dataset.
 print("Dataset registered successfully.")
 
 print("All expected columns are present.")
 print(f"\nDataset shape: {df.shape}")
 
+print("\n-----------------------------------------------------------------------------------")
+
 print("\nColumn names:")
 print(df.columns.tolist())
+print("\n-----------------------------------------------------------------------------------")
+
+print("\nData types:")
+print(df.info())
+print("\n-----------------------------------------------------------------------------------")
+
+print("\nDuplicate values:")
+print(df.duplicated().sum())
+print("\n-----------------------------------------------------------------------------------")
 
 print("\nMissing values:")
 print(df.isnull().sum())
+print("\n-----------------------------------------------------------------------------------")
 
 print("\nTarget distribution (ProdTaken):")
 print(df["ProdTaken"].value_counts())
+print("\n-----------------------------------------------------------------------------------")
 
 print("\nDataset summary:")
 print(df.describe(include="all"))
+print("\n-----------------------------------------------------------------------------------")
